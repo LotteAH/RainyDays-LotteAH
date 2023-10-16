712ed67a8a171312f8f6f7129b4fd13c2c8419c8
LotteAH <117653989+LotteAH@users.noreply.github.com>
2023-10-16 14:50:24 +0200

    Added js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const resultsContainer = document.getElementById('womens_results');
+
 
 const url = "https://api.noroff.dev/api/v1/rainy-days";
 
@@ -5,18 +7,39 @@ async function getJackets() {
 
   const response = await fetch(url); 
 
-  const results = awaitresponse.json();
+  const jackets = await response.json();
 
-  const facts = results.all;
 
   resultsContainer.innerHTML = "";
   
-  for(let i = 0; < rainy-days.length; i++) {
-      console.log(id[i].text);
+  for(let i = 0; i < jackets.length; i++) {
+      console.log(jackets[i]);
 
-      resultsContainer.innerHTML += `<div class"result">${rainy-days[i].text}</div>`;
+      resultsContainer.innerHTML += `<div class="container products">
+                                      <div class"result">${jackets[i].image}</div>
+                                      <div class"result">${jackets[i].title}</div>
+                                      <div class"result">${jackets[i].baseColor}</div>
+                                      <div class"result">${jackets[i].description}</div>
+                                      <div class"result">${jackets[i].price}</div>           
+                                      <a href="oslo_urban.html"
+                                      ><img
+                                      src="images/womens_1.jpg"
+              alt="Oslo Urban Anorak"
+              class="product_img_men"
+          /></a>
+          <h3>${jackets[i].title}</h3>
+          <h4>${jackets[i].baseColor}</h4>
+          <h5>${jackets[i].description}</h5>
+          <p class="price">${jackets[i].price}</p>
+          <a href="oslo_urban.html"> </a>
+        </div>
+      `;
   }
 }
-console.log(getJackets);
+
 
 getJackets();
+
+
+// hello, it's Talitha here 
+// Hi :)
