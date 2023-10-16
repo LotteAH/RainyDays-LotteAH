76caaa6e95f98f8246369f9f8574fa821bd810c6
LotteAH <117653989+LotteAH@users.noreply.github.com>
2023-10-16 09:26:15 +0200

    changes on js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,17 @@
 
-function fetchAkraJacketData() {
-    const apiUrl = "https://api.noroff.dev/api/v1/rainy-days";
+const url = "https://api.noroff.dev/api/v1/rainy-days";
+
+async function getJackets() {
+
+  const response = await fetch(url); 
+
+  const results = awaitresponse.json();
+
+  const facts = results.all;
   
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        for (const product of data) {
-            console.log("Title:", product.title);
-            console.log("Description:", product.description);
-            console.log("Gender:", product.gender);
-            console.log("Sizes:", product.sizes.join(", "));
-            console.log("Base Color:", product.baseColor);
-            console.log("Price:", product.price);
-            console.log("Discounted Price:", product.discountedPrice);
-            console.log("On Sale:", product.onSale);
-            console.log("Image:", product.image);
-            console.log("Tags:", product.tags.join(", "));
-            console.log("Favorite:", product.favorite);
-        }
-      })
-      .catch(error => {
-        console.error("An error occurred while fetching data:", error);
-      });
+  for(let i = 0; < facts.length; i++) {
+    
+  }
 }
 
-fetchAkraJacketData();
+getJackets();
