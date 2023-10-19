6c956ea7697115953fe1ee2b93499f9a56e207a4
LotteAH <117653989+LotteAH@users.noreply.github.com>
2023-10-19 09:01:26 +0200

    edit

--- js/script.js
+++ js/script.js
@@ -9,6 +9,20 @@ async function getJackets() {
 
   const jackets = await response.json();
 
+  // fetch('https://api.example.com/images')
+  // .then(response => response.json())
+  // .then(data => {
+  //   const container = document.getElementById('image-container');
+  //   data.forEach(image => {
+  //     const imageElement = document.createElement('img');
+  //     imageElement.src = image.url;
+  //     container.appendChild(imageElement);
+  //   });
+  // })
+  // .catch(error => {
+  //   console.error('Error fetching data from the API: ', error);
+  // });
+
 
   resultsContainer.innerHTML = "";
   
@@ -16,25 +30,16 @@ async function getJackets() {
       console.log(jackets[i]);
 
       resultsContainer.innerHTML += `<div class="container products">
-                                      <div class"result">${jackets[i].image}</div>
-                                      <div class"result">${jackets[i].title}</div>
-                                      <div class"result">${jackets[i].baseColor}</div>
-                                      <div class"result">${jackets[i].description}</div>
-                                      <div class"result">${jackets[i].price}</div>           
-                                      <a href="oslo_urban.html"
-                                      ><img
-                                      src="images/womens_1.jpg"
-              alt="Oslo Urban Anorak"
-              class="product_img_men"
-          /></a>
-          <h3>${jackets[i].title}</h3>
-          <h4>${jackets[i].baseColor}</h4>
-          <h5>${jackets[i].description}</h5>
-          <p class="price">$ ${jackets[i].price}</p>
-          <a href="oslo_urban.html"> </a>
-        </div>
-      `;
-  }
+                                      <div class"result">${jackets[i].image}</div> 
+                                      <div id="image-container"></div>
+                                      <h3>${jackets[i].title}</h3>
+                                      <h4>${jackets[i].baseColor}</h4>
+                                      <h5>${jackets[i].description}</h5>
+                                      <p class="price">$ ${jackets[i].price}</p>
+                                      <a href="oslo_urban.html"> </a>
+                                      </div>
+                                      `;
+                                      }
 }
 
 
@@ -43,3 +48,11 @@ getJackets();
 
 // hello, it's Talitha here 
 // Hi :)
+
+
+  //                             <a href="oslo_urban.html"
+          //                             ><img
+          //                             src="images/womens_1.jpg"
+          //     alt="Oslo Urban Anorak"
+          //     class="product_img_men"
+          // /></a>
