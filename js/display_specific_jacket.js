21059eb74a14ac1cd907468111757d7d471ea91a
LotteAH <117653989+LotteAH@users.noreply.github.com>
2023-10-23 13:27:40 +0200

    Update display_specific_jacket.js

--- js/display_specific_jacket.js
+++ js/display_specific_jacket.js
@@ -3,59 +3,23 @@ const resultsContainer = document.getElementById("womens_results");
  
 const url = "https://api.noroff.dev/api/v1/rainy-days";
  
-async function getWomensJackets() {
-// showLoadingIndicator();
-  const response = await fetch(url);
-  const jackets = await response.json();
-  return jackets;
-}
- 
-async function displayJackets() {
-  const jackets = await getWomensJackets();
-  const resultsContainer = document.getElementById("womens_results");
-  resultsContainer.innerHTML = "";
- 
-  for (let i = 0; i < jackets.length; i++) {
-    if (jackets[i].gender == "Female") {
-      console.log(jackets[i]);
-    
-      resultsContainer.innerHTML += `<div class="container products ">
-                                          <div class"result"><img src="${jackets[i].image}" alt="${jackets[i].description} class="product_img_men"/></div>
-                                          <div id="image-container"></div>
-                                          <h3>${jackets[i].title}</h3>
-                                          <h4>${jackets[i].baseColor}</h4>
-                                          <p class="price">$ ${jackets[i].price}</p>
-                                          <a href="specific_jacket.html"> </a>
-                                          </div>
-                                          `;
-      resultsContainer.addEventListener("click", () => {
-        window.location.href = `specific_jacket.html?id=${jackets[i].id}`;
-      });
-    }
-  }
-}
-
-getWomensJackets();
-
-const resultsContainer = document.getElementById("specific_jacket");
-
-const url = "https://api.noroff.dev/api/v1/rainy-days";
 
 
  function getSpecificJacketIdFromQuery() {
-    const urlParams = new URLSearchParams(window.location.search);
+    const urlParams = new URLSearchParams(window.location);
     return urlParams.get("id");
  }  
 
-  async function displaySpecificJacket(){
-    const jackets = getSpecificJacketIdFromQuery;(); {
-      if (!specific_jacketId) {
+  async function displaySpecificJacket() {
+    const jacketId = getSpecificJacketIdFromQuery(); {
+      if (!jacketId) {
         return;
       }
     
-      const specificJacketResult = await response.json(); {
-  
-      const specificJacket = document.getElementById("specific_jacket");
+      const response = await fetch(`https://api.noroff.dev/api/v1/rainy-days/${jacketsId}`); 
+      const jackets = await response.json();
+
+      const resultsContainer = document.getElementById("specific_jacket");
       resultsContainer.innerHTML += `<div class="container products">
                                           <div class"result"><img src="${jackets[i].image}" alt="${jackets[i].description} class="product_img_men"/></div>
                                           <div id="image-container"></div>
@@ -66,7 +30,6 @@ const url = "https://api.noroff.dev/api/v1/rainy-days";
                                           <p class="price">$ ${jackets[i].price}</p>
                                           `; 
   }
-}
 }
   displaySpecificJacket();
   
@@ -104,3 +67,41 @@ const url = "https://api.noroff.dev/api/v1/rainy-days";
  // } catch (error) {
     //   showError(error.message);
     // }
+
+    // async function getWomensJackets() {
+// // showLoadingIndicator();
+//   const response = await fetch(url);
+//   const jackets = await response.json();
+//   return jackets;
+// }
+ 
+// async function displayJackets() {
+//   const jackets = await getWomensJackets();
+//   const resultsContainer = document.getElementById("womens_results");
+//   resultsContainer.innerHTML = "";
+ 
+//   for (let i = 0; i < jackets.length; i++) {
+//     if (jackets[i].gender == "Female") {
+//       console.log(jackets[i]);
+    
+//       resultsContainer.innerHTML += `<div class="container products ">
+//                                           <div class"result"><img src="${jackets[i].image}" alt="${jackets[i].description} class="product_img_men"/></div>
+//                                           <div id="image-container"></div>
+//                                           <h3>${jackets[i].title}</h3>
+//                                           <h4>${jackets[i].baseColor}</h4>
+//                                           <p class="price">$ ${jackets[i].price}</p>
+//                                           <a href="specific_jacket.html"> </a>
+//                                           </div>
+//                                           `;
+//       resultsContainer.addEventListener("click", () => {
+//         window.location.href = `specific_jacket.html?id=${jackets[i].id}`;
+//       });
+//     }
+//   }
+// }
+
+// getWomensJackets();
+
+// const resultsContainer = document.getElementById("specific_jacket");
+
+// const url = "https://api.noroff.dev/api/v1/rainy-days";
