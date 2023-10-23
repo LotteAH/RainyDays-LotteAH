e8ba448adeac87561358db538fabb0c1307bf17d
LotteAH <117653989+LotteAH@users.noreply.github.com>
2023-10-23 13:10:42 +0200

    Update display_specific_jacket.js

--- js/display_specific_jacket.js
+++ js/display_specific_jacket.js
@@ -1,4 +1,42 @@
 
+const resultsContainer = document.getElementById("womens_results");
+ 
+const url = "https://api.noroff.dev/api/v1/rainy-days";
+ 
+async function getWomensJackets() {
+// showLoadingIndicator();
+  const response = await fetch(url);
+  const jackets = await response.json();
+  return jackets;
+}
+ 
+async function displayJackets() {
+  const jackets = await getWomensJackets();
+  const resultsContainer = document.getElementById("womens_results");
+  resultsContainer.innerHTML = "";
+ 
+  for (let i = 0; i < jackets.length; i++) {
+    if (jackets[i].gender == "Female") {
+      console.log(jackets[i]);
+    
+      resultsContainer.innerHTML += `<div class="container products ">
+                                          <div class"result"><img src="${jackets[i].image}" alt="${jackets[i].description} class="product_img_men"/></div>
+                                          <div id="image-container"></div>
+                                          <h3>${jackets[i].title}</h3>
+                                          <h4>${jackets[i].baseColor}</h4>
+                                          <p class="price">$ ${jackets[i].price}</p>
+                                          <a href="specific_jacket.html"> </a>
+                                          </div>
+                                          `;
+      resultsContainer.addEventListener("click", () => {
+        window.location.href = `specific_jacket.html?id=${jackets[i].id}`;
+      });
+    }
+  }
+}
+
+getWomensJackets();
+
 const resultsContainer = document.getElementById("specific_jacket");
 
 const url = "https://api.noroff.dev/api/v1/rainy-days";
