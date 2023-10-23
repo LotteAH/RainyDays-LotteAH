cf88de9d240f8a80c52764a64bece123e7e57b3f
LotteAH <117653989+LotteAH@users.noreply.github.com>
2023-10-23 12:42:43 +0200

    Update display_specific_jacket.js

--- js/display_specific_jacket.js
+++ js/display_specific_jacket.js
@@ -5,12 +5,31 @@ const url = "https://api.noroff.dev/api/v1/rainy-days";
 
 
  function getSpecificJacketIdFromQuery() {
-    const urlParams = new URLSearchParams(window.location);
-    const id = urlParams.get("id");
-    return id;
- }
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get("id");
+ }  
 
-    // async function fetchSpecificJacket() {
+  async function displaySpecificJacket(){
+    const jackets = getSpecificJacketIdFromQuery; {
+    const specificJacketResult = await response.json(); {
+  
+      const specificJacket = document.getElementById("specific_jacket");
+      resultsContainer.innerHTML += `<div class="container products">
+                                          <div class"result"><img src="${jackets[i].image}" alt="${jackets[i].description} class="product_img_men"/></div>
+                                          <div id="image-container"></div>
+                                          <h3>${jackets[i].title}</h3>
+                                          <h4>${jackets[i].baseColor}</h4>
+                                          <h4>${jackets[i].size}</h4>
+                                          <h5>${jackets[i].description}</h5>
+                                          <p class="price">$ ${jackets[i].price}</p>
+                                          `; 
+  }
+}
+}
+  displaySpecificJacket();
+  
+
+   // async function fetchSpecificJacket() {
     //     const specificJacket = getSpecificJacketIdFromQuery();
     //     if(!specificJacket) {
     //         return;
@@ -43,24 +62,3 @@ const url = "https://api.noroff.dev/api/v1/rainy-days";
  // } catch (error) {
     //   showError(error.message);
     // }
-
-  async function displaySpecificJacket(){
-    const jackets = getSpecificJacketIdFromQuery; {
-    const specificJacketResult = await response.json(); {
-  
-      const specificJacket = document.getElementById("specific_jacket");
-      resultsContainer.innerHTML += `<div class="container products ">
-                                          <div class"result"><img src="${jackets[i].image}" alt="${jackets[i].description} class="product_img_men"/></div>
-                                          <div id="image-container"></div>
-                                          <h3>${jackets[i].title}</h3>
-                                          <h4>${jackets[i].baseColor}</h4>
-                                          <h4>${jackets[i].size}</h4>
-                                          <h5>${jackets[i].description}</h5>
-                                          <p class="price">$ ${jackets[i].price}</p>
-                                          `;
-   
-  }
-}
-  }
-  displaySpecificJacket();
-  
