3872f3a45055374fdcd0a26b168bebbb760a29f4
LotteAH <117653989+LotteAH@users.noreply.github.com>
2023-10-23 12:30:37 +0200

    deleted changes

--- js/womens_script.js
+++ js/womens_script.js
@@ -3,22 +3,18 @@ const resultsContainer = document.getElementById("womens_results");
 const url = "https://api.noroff.dev/api/v1/rainy-days";
 
 async function getWomensJackets() {
-showLoadingIndicator();
+    // showLoadingIndicator();
   const response = await fetch(url);
+  // clearLoadingIndicator();
   const jackets = await response.json();
-  return jackets;
-}
 
-async function displayJackets() {
-  const jackets = await getWomensJackets();
-  const resultsContainer = document.getElementById("womens_results");
   resultsContainer.innerHTML = "";
 
   for (let i = 0; i < jackets.length; i++) {
     if (jackets[i].gender == "Female") {
       console.log(jackets[i]);
-    
-      resultsContainer.innerHTML += `<div class="container products ">
+
+      resultsContainer.innerHTML += `<div class="container products">
                                           <div class"result"><img src="${jackets[i].image}" alt="${jackets[i].description} class="product_img_men"/></div>
                                           <div id="image-container"></div>
                                           <h3>${jackets[i].title}</h3>
@@ -29,11 +25,12 @@ async function displayJackets() {
                                           `;
       resultsContainer.addEventListener("click", () => {
         window.location.href = `specific_jacket.html?id=${jackets[i].id}`;
-      });
-      resultsContainer.appendChild(resultsContainer);
+      })
     }
   }
 }
+getWomensJackets();
+
   //     resultsContainer.appendChild(jackets[i]);
   //   }
   // }
@@ -54,12 +51,3 @@ async function displayJackets() {
 // }
 // }
 //   }
-
-  function showLoadingIndicator() {
-    const jackets = document.getElementByClass("loader");
-    itemList.innerHTML = "loader";
-  }
-
-getWomensJackets();
-
-
