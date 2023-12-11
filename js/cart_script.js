b9f7ff0b9574442875e67e5b2e66b49c077dc287
LotteAH <117653989+LotteAH@users.noreply.github.com>
2023-12-11 14:46:47 +0100

    edited

--- js/cart_script.js
+++ js/cart_script.js
@@ -3,9 +3,8 @@ const loadingIndicator = document.getElementById("loading-indicator");
 const url = "https://api.noroff.dev/api/v1/rainy-days";
 
 function getCartItems() {
-    return JSON.parse(localStorage.getItem("cart_item")) || [];
-   
-  }
+  return JSON.parse(localStorage.getItem("cart_items")) || [];
+}
   
   async function fetchCartDetails(productId) {
     showLoadingIndicator(); 
@@ -22,7 +21,7 @@ function getCartItems() {
     hideLoadingIndicator();
     
     resultsContainer.innerHTML = `
-      <div id="specific_jacket">
+      <div id="fetchCartDetails">
         <div class="container_specific_jacket">
           <div class="selected_image_products">
             <img src="${jackets.image}" alt="${jackets.description}" />
@@ -48,19 +47,19 @@ function getCartItems() {
   }
 }
 
-  function displayCartItems() {
-    const cartContainer = document.querySelector(".cart_container");
-    const cartItems = getCartItems();
-  
-    if (cartItems.length === 0) {
-      cartContainer.innerHTML = "<p>Your cart is empty.</p>";
-    } else {
-      cartContainer.innerHTML = "";
-  
-      cartItems.forEach((item) => {
-        const cartHTML = `
+function displayCartItems() {
+  const cartContainer = document.getElementById("cart_item");
+  const cartItems = getCartItems();
+
+  if (cartItems.length === 0) {
+    cartContainer.innerHTML = "<p>Your cart is empty.</p>";
+  } else {
+    cartContainer.innerHTML = "";
+
+    cartItems.forEach((item) => {
+      const cartHTML = `
           <div class="cart_item">
-          <a href="cart.html?id=${jacketId}">
+          <a href="cart.html?id=${fetchCartDetailsId}">
             <div class="result">
             <img src="${jackets.image}" alt="${jackets.description}" />
             </div>
@@ -74,33 +73,46 @@ function getCartItems() {
           </div>
         `;
         cartContainer.innerHTML += cartHTML;
-      });
-    }
+    });
   }
+}
+
   
 
-  function addToCart(item) {
-    const cartItems = getCartItems();
-    cartItems.push(item);
-    localStorage.setItem("cart", JSON.stringify(cartItems));
-    displayCartItems();
-  }
+function addToCart(item) {
+  const cartItems = getCartItems();
+  cartItems.push(item);
+  localStorage.setItem("cart_items", JSON.stringify(cartItems));
+  displayCartItems();
+}
   
-  async function fetchCartDetails(productId) {
-    const url = `https://api.noroff.dev/api/v1/rainy-days/${productId}`;
-  
-    try {
-      const response = await fetch(url);
-  
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-  
-      const productDetails = await response.json()
+async function fetchCartDetails(productId) {
+  const url = `https://api.noroff.dev/api/v1/rainy-days/${productId}`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const productDetails = await response.json();
       const cartElement = document.getElementById('cart');
       cartElement.innerHTML = `
-        <!-- Your product details HTML structure using 'productDetails' -->
-      `;
+      <div class="cart_item">
+      <a href="cart.html?id=${fetchCartDetailsId}">
+        <div class="result">
+        <img src="${jackets.image}" alt="${jackets.description}" />
+        </div>
+        <div class="product_info">
+          <h3>${jackets.title}</h3>
+          <h4>${jackets.baseColor}</h4>
+          <h5>${jackets.description}</h5>
+          <p class="price">$ ${jackets.price}</p>
+          <button class="cta-blue_cart" id="addToCartBtn">ADD TO CART</button>
+        </div>
+      </div>
+    `;
   
     } catch (error) {
       console.error("An error occurred:", error);
@@ -111,7 +123,7 @@ function getCartItems() {
     displayCartItems();
   
     const urlParams = new URLSearchParams(window.location.search);
-    const productId = urlParams.get('id');
+    const productId = urlParams.get("id");
   
     if (productId) {
       fetchCartDetails(productId);
