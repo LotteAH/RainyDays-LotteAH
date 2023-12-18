66bfad9ac3b317fafb0ae6e1639e43c2438a77a9
Fermain <Fermain@users.noreply.github.com>
2023-12-18 15:33:40 +0200

    Feedback

--- js/cart_script.js
+++ js/cart_script.js
@@ -1,134 +1,40 @@
+import { showLoadingIndicator, hideLoadingIndicator } from "./indicator.js";
+
 const resultsContainer = document.getElementById("cart_item");
-const loadingIndicator = document.getElementById("loading-indicator");
+
 const url = "https://api.noroff.dev/api/v1/rainy-days";
 
 function getCartItems() {
-  return JSON.parse(localStorage.getItem("cart_item")) || [];
-}
-  
-  async function fetchCartDetails(productId) {
-    showLoadingIndicator(); 
-  const url = `https://api.noroff.dev/api/v1/rainy-days/${productId}`;
-
-  try {
-    const response = await fetch(url);
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    const productDetails = await response.json();
-    hideLoadingIndicator();
-    
-    resultsContainer.innerHTML = `
-      <div id="fetchCartDetails">
-        <div class="container_specific_jacket">
-          <div class="selected_image_products">
-            <img src="${jackets.image}" alt="${jackets.description}" />
-          </div>
-          <div class="product_info">
-            <h3>${jackets.title}</h3>
-            <h4>${jackets.baseColor}</h4>
-            <h5>${jackets.description}</h5>
-            <p class="price">$ ${jackets.price}</p>
-            <button class="cta-green cta-cart" id="addToCartBtn">CHECK OUT</button>
-            <button class="cta-blue cta-cart">CONTINUE SHOPPINGT</button>
-          </div>
-        </div>
-      </div>
-    `;
-
-    addToCart(item);
-
-  } catch (error) {
-    console.error("An error occurred:", error);
-  }
+  return JSON.parse(localStorage.getItem("cart")) || [];
 }
 
-function displayCartItems() {
+function createCartItem(item) {
   const cartContainer = document.getElementById("cart_item");
-  const cartItems = getCartItems();
 
-  if (cartItems.length === 0) {
-    cartContainer.innerHTML = "<p>Your cart is empty.</p>";
-  } else {
-    cartContainer.innerHTML = "";
-
-    cartItems.forEach((item) => {
-      const cartHTML = `
+  const cartHTML = `
           <div class="cart_item">
-          <a href="cart.html?id=${fetchCartDetailsId}">
+          <a href="cart.html?id=${item.id}">
             <div class="result">
-            <img src="${jackets.image}" alt="${jackets.description}" />
+            <img src="${item.image}" alt="${item.description}" />
             </div>
             <div class="product_info">
-              <h3>${jackets.title}</h3>
-              <h4>${jackets.baseColor}</h4>
-              <h5>${jackets.description}</h5>
-              <p class="price">$ ${jackets.price}</p>
-              <button class="cta-green cta-cart" id="addToCartBtn">CHECK OUT</button>
+              <h3>${item.title}</h3>
+              <h4>${item.baseColor}</h4>
+              <h5>${item.description}</h5>
+              <p class="price">$ ${item.price}</p>
+              <a href="/checkout.html" class="cta-green cta-cart" id="addToCartBtn">CHECK OUT</a>
               <button class="cta-blue cta-cart">CONTINUE SHOPPINGT</button>
             </div>
           </div>
         `;
-        cartContainer.innerHTML += cartHTML;
-    });
-  }
+
+  cartContainer.innerHTML += cartHTML;
 }
 
-  
-
-function addToCart(item) {
-  const cartItems = getCartItems();
-  cartItems.push(item);
-  localStorage.setItem("cart_items", JSON.stringify(cartItems));
-  displayCartItems();
+function initCartPage() {
+  const cart = getCartItems();
+  cart.forEach(createCartItem);
+  hideLoadingIndicator();
 }
-  
-async function fetchCartDetails(productId) {
-  const url = `https://api.noroff.dev/api/v1/rainy-days/${productId}`;
 
-  try {
-    const response = await fetch(url);
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    const productDetails = await response.json();
-      const cartElement = document.getElementById('cart');
-      cartElement.innerHTML = `
-      <div class="cart_item">
-      <a href="cart.html?id=${fetchCartDetailsId}">
-        <div class="result">
-        <img src="${jackets.image}" alt="${jackets.description}" />
-        </div>
-        <div class="product_info">
-          <h3>${jackets.title}</h3>
-          <h4>${jackets.baseColor}</h4>
-          <h5>${jackets.description}</h5>
-          <p class="price">$ ${jackets.price}</p>
-          <button class="cta-green cta-cart" id="addToCartBtn">CHECK OUT</button>
-          <button class="cta-blue cta-cart">CONTINUE SHOPPINGT</button>
-        </div>
-      </div>
-    `;
-  
-    } catch (error) {
-      console.error("An error occurred:", error);
-    }
-  }
-  
-  function initCartPage() {
-    displayCartItems();
-  
-    const urlParams = new URLSearchParams(window.location.search);
-    const productId = urlParams.get("id");
-  
-    if (productId) {
-      fetchCartDetails(productId);
-    }
-  }
-  
-  initCartPage();
-  
+initCartPage();
