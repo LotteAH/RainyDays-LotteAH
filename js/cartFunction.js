eb930ba51f17e8ebe1b31c87bd83e307252127ee
LotteAH <117653989+LotteAH@users.noreply.github.com>
2024-01-10 09:40:22 +0100

    Cart number

--- js/cartFunction.js
+++ js/cartFunction.js
@@ -1,17 +1,19 @@
-
 function getCartItems() {
-    return JSON.parse(localStorage.getItem("cart")) || [];
-  }
-  
-  
-  function updateCartItemsCount() {
-    const cartItems = getCartItems();
-    const cartItemsCountElement = document.getElementById("cart-items-count");
-  
-    if (cartItemsCountElement) {
+  return JSON.parse(localStorage.getItem("cart")) || [];
+}
+
+function updateCartItemsCount() {
+  const cartItems = getCartItems();
+  const cartItemsCountElement = document.getElementById("cart-items-count");
+
+  if (cartItemsCountElement) {
+    if (cartItems.length === 0) {
+      cartItemsCountElement.style.display = "none"; // Hide the count element
+    } else {
       cartItemsCountElement.textContent = cartItems.length.toString();
+      cartItemsCountElement.style.display = "block"; // Show the count element
     }
   }
-  
-  updateCartItemsCount();
-  
+}
+
+updateCartItemsCount();
